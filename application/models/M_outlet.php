826e145c2faadfddef78d4402cb4f3129b61696e
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-11-07 16:01:54 +0700

    outelet

---
+++ application/models/M_outlet.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class M_outlet extends CI_Model
+{
+	public function outlet()
+	{
+		$this->db->select('*');
+		$this->db->from('outlet');
+		$this->db->join('user', 'outlet.id_user = user.id_user', 'left');
+		$this->db->where('outlet.id_user', $this->session->userdata('id_user'));
+
+		return $this->db->get()->result();
+	}
+}
+
+/* End of file M_transaksi.php */
