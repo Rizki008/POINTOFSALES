f6d548867a440053331967cd406ffa500ca31153
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2023-11-14 19:21:32 +0700

    crud outlet

--- application/models/M_outlet.php
+++ application/models/M_outlet.php
@@ -21,6 +21,37 @@ class M_outlet extends CI_Model
 		$this->db->join('user', 'outlet.id_user = user.id_user', 'left');
 		return $this->db->get()->result();
 	}
+
+	public function outletadmin()
+	{
+		$this->db->select('*');
+		$this->db->from('outlet');
+		$this->db->join('user', 'outlet.id_user = user.id_user', 'left');
+		return $this->db->get()->result();
+	}
+	public function user()
+	{
+		$this->db->select('*');
+		$this->db->from('user');
+		$this->db->where('level_user', 'kasir');
+		return $this->db->get()->result();
+	}
+	public function add($data)
+	{
+		$this->db->insert('outlet', $data);
+	}
+
+	public function edit($data)
+	{
+		$this->db->where('id_outlet', $data['id_outlet']);
+		$this->db->update('outlet', $data);
+	}
+
+	public function delete($data)
+	{
+		$this->db->where('id_outlet', $data['id_outlet']);
+		$this->db->delete('outlet', $data);
+	}
 }
 
 /* End of file M_transaksi.php */
